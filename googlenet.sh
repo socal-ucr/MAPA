a5f2a3f7953082da621578f33ee366d43f76590b
Kiran Ranganath <kiran7193@gmail.com>
2021-01-12 15:57:21 -0800

    Add Caffe submodule.

--- googlenet.sh
+++ googlenet.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "GoogleNet $@"
+GPUS=$@
+echo "GoogleNet $GPUS"
+cd caffe
+./build/tools/caffe train --solver=caffe-models/bvlc_googlenet/solver.prototxt --gpu $GPUS
--- vgg16.sh
+++ vgg16.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "VGG-16 $@"
+GPUS=$@
+echo "VGG-16 $GPUS"
+cd caffe
+./build/tools/caffe train --solver=caffe-models/vgg-16/solver.prototxt --gpu $GPUS
